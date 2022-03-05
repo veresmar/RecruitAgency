98e2507f9cdec082375c01b5e27af9f10debf284
Maria Veres <veres.maria.dev@gmail.com>
2022-03-05 13:14:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// ACCORDION
+document.querySelectorAll('.accordion_item_trigger').forEach((item) =>
+  item.addEventListener('click',() => {
+      const parent = item.parentNode;
+
+      if (parent.classList.contains('accordion_item--active')) {
+        parent.classList.remove('accordion_item--active');
+      } else {
+        document
+              .querySelectorAll('.accordion_item')
+              .forEach((child) => child.classList.remove('accordion_item--active'))
+        
+        parent.classList.add('accordion_item--active');
+      }
+  })
+)
+
+
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel();
+// });
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:1000,
+  margin:10,
+  nav:false,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      },
+      1400:{
+        items:4
+    }
+  }
+})
